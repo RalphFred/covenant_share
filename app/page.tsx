1fb3aa9485d4390c3404ea88293cc5ac5cc496fd
RalphFred <raphaelgbaorun@gmail.com>
2024-12-31 06:37:15 +0100

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia at perspiciatis fuga quis nobis, sapiente ipsum itaque ducimus delectus doloribus.
+    </div>
+  );
+}
