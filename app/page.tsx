9b8740a1471e95566263646fdcf02a1cb61b3b6a
RalphFred <raphaelgbaorun@gmail.com>
2025-01-02 22:17:05 +0100

    done

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
  
   return (
      <div className="lg:px-24">
-      <p className="text-lg mb-8">Looking to share a ride? Enter your details below and connect with others moving from the airport to school around the same time as you.</p>
+      <p className="text-lg mb-8">Looking to share a ride? Enter your details below and connect with others moving from the anywhere to school around the same time as you.</p>
       <Form />
      </div>
   );
